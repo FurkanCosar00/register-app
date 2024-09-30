8d6d1b620d9dcb922532b850112aba0ddf3d8c53
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-09-30 23:11:08 +0300

    stepler hazırlandı

--- app/page.jsx
+++ app/page.jsx
@@ -4,29 +4,46 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <form action={async(formData) => {
-                console.log(formData);
-                const formObj = Object.fromEntries(formData);
-                console.log(formObj);
+      <form action={async (formData) => {
+        console.log(formData);
+        const formObj = Object.fromEntries(formData);
+        console.log(formObj);
 
-                const response = await BasvuruYapAksiyonu(formObj);
-                console.log(response);
+        const response = await BasvuruYapAksiyonu(formObj);
+        console.log(response);
 
-                if(response?.errors){
-                    setErrorsState({
-                        errors: response?.errors
-                    })
-                    return;
-                }
+        if (response?.errors) {
+          setErrorsState({
+            errors: response?.errors
+          })
+          return;
+        }
 
-                setStep(prev => prev + 1)
-                
-                
-            }} className="form-group">
-        <label htmlFor="deneme">
-          <p>deneme inputu</p>
-          <input type="text" name="deneme" />
-        </label>
+        setStep(prev => prev + 1)
+
+
+      }} className="form-group">
+
+        <input type="hidden" name="step" value={step} />
+        {step === 1 && (
+          <>
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+          </>
+        )}
+
+        {step === 3 && (
+          <>
+          </>
+        )}
+
+        {step === 4 && (
+          <>
+          </>
+        )}
       </form>
     </>
   );
