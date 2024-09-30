58d0c1bcedc4c114b01f0b633c4f806c666653e4
Furkan <furkancosar2005@gmail.com>
2024-09-30 23:01:13 +0300

    deneme commiti

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,12 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-    
+      <form action="" className="form-group">
+        <label htmlFor="deneme">
+          <p>deneme inputu</p>
+          <input type="text" name="deneme" />
+        </label>
+      </form>
     </>
   );
 }
