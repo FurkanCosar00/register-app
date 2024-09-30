20c3bee034e95cf730c69844154443fdb05d1dcb
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-09-30 23:05:19 +0300

    form action oluşturuldu

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,25 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <form action="" className="form-group">
+      <form action={async(formData) => {
+                console.log(formData);
+                const formObj = Object.fromEntries(formData);
+                console.log(formObj);
+
+                const response = await BasvuruYapAksiyonu(formObj);
+                console.log(response);
+
+                if(response?.errors){
+                    setErrorsState({
+                        errors: response?.errors
+                    })
+                    return;
+                }
+
+                setStep(prev => prev + 1)
+                
+                
+            }} className="form-group">
         <label htmlFor="deneme">
           <p>deneme inputu</p>
           <input type="text" name="deneme" />
