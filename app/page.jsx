061789fbf1bbcf98992be0d09c0d33ab849783b7
fatih can kaya <fatihhkayaaaa@gmail.com>
2024-10-02 22:02:12 +0300

    focusing inputu tamamlandı

--- app/page.jsx
+++ app/page.jsx
@@ -98,6 +98,7 @@ export default function Home() {
             <p>Öğrenci derslere katılım sağladı mı? <input name="lesson" type="radio" id="lessonYes" /> <label htmlFor="lessonYes">evet</label>    <input name="lesson" type="radio" id="lessonNo" /> <label htmlFor="lessonNo">Hayır</label></p>
             <p>Öğrenci eğitmenleri söylemediği halde proje geşiltirdi mi? <input name="project" type="radio" id="projectYes" /> <label htmlFor="projectYes">evet</label>   <input name="project" type="radio" id="projectNo" /><label htmlFor="projectNo">hayır</label></p>
             <p>Öğrenci derse herzaman tam saatinde geldi mi? <input name="lessonTime" type="radio" id="lessonTimeYes" /> <label htmlFor="lessonTimeYes">evet</label>    <input name="lessonTime" type="radio" id="lessonTimeNo"  /><label htmlFor="lessonTimeNo">hayır</label></p>
+            <p>Öğrenci ders esnasında sadece derse odaklandı mı?  <input name="focusing" type="radio" id="focusingYes" /> <label htmlFor="focusingYes">evet</label>  <input name="focusing" type="radio" id="focusing"No /> <label htmlFor="focusingNo">hayır</label></p>
           </>
         )}
       </form>
